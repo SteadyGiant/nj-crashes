b186187c41f13a42aa4d1fdab1d095091358da90
glassbro <glassbro@pm.me>
2023-03-10 09:05:59 -0500

    Note how to install tabulizer.

--- extract.r
+++ extract.r
@@ -1,4 +1,6 @@
 library(readr)
+# install.packages("rJava")
+# remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"))
 library(tabulizer)
 
 tbl = tabulizer::extract_tables("data/raw/ptccr_22.pdf", output = "data.frame")
