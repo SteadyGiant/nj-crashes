490090b0890a0da695a4229d91c54b8f3e13179d
glassbro <glassbro@pm.me>
2023-03-10 18:03:14 -0500

    Add state/county FIPS code. Rearrange columns.

--- main.r
+++ main.r
@@ -6,27 +6,27 @@ options(scipen = 999)
 
 tidycensus::census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
+fat = readr::read_csv("data/staging/ptccr_22.csv") %>%
+  dplyr::mutate(`Cyclist/Pedestrian Fatalities (2022)` = Pedalcyclist + Pedestrian) %>%
+  dplyr::select(County, `Cyclist/Pedestrian Fatalities (2022)`, `Total Fatalities (2022)` = Fatalities)
+
 state_pop = pop = tidycensus::get_decennial(
   geography = "state", variables = "P1_001N", year = 2020, sumfile = "pl", state = "NJ"
 ) %>%
   dplyr::mutate(NAME = dplyr::case_match(NAME, "New Jersey" ~ "Total")) %>%
-  dplyr::select(County = NAME, `Population (2020)` = value)
+  dplyr::select(GEOID, County = NAME, `Population (2020)` = value)
 
 county_pop = tidycensus::get_decennial(
   geography = "county", variables = "P1_001N", year = 2020, sumfile = "pl", state = "NJ"
 ) %>%
   dplyr::mutate(County = gsub(" County, New Jersey", "", NAME)) %>%
-  dplyr::select(County, `Population (2020)` = value) %>%
-  dplyr::union(state_pop)
-
-fat = readr::read_csv("data/staging/ptccr_22.csv") %>%
-  dplyr::mutate(`Cyclist/Pedestrian Fatalities (2022)` = Pedalcyclist + Pedestrian) %>%
-  dplyr::select(County, `Cyclist/Pedestrian Fatalities (2022)`, `Fatalities (2022)` = Fatalities) %>%
-  dplyr::left_join(county_pop, by = "County") %>%
+  dplyr::select(GEOID, County, `Population (2020)` = value) %>%
+  dplyr::union(state_pop) %>%
+  dplyr::left_join(fat, by = "County") %>%
   dplyr::mutate(
-    `Fatality Rate` = (`Fatalities (2022)` / `Population (2020)`) * 100000,
-    `Cyclist/Pedestrian Fatality Rate` = (`Cyclist/Pedestrian Fatalities (2022)` / `Population (2020)`) * 100000
+    `Cyclist/Pedestrian Fatality Rate` = (`Cyclist/Pedestrian Fatalities (2022)` / `Population (2020)`) * 100000,
+    `Fatality Rate` = (`Total Fatalities (2022)` / `Population (2020)`) * 100000
   ) %>%
   dplyr::arrange(dplyr::desc(`Fatality Rate`))
 
-readr::write_csv(fat, "data/final/fatalities_2022.csv")
+readr::write_csv(county_pop, "data/final/fatalities_2022.csv")
